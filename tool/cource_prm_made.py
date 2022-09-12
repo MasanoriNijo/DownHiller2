4422d5be00eebcb9abee6c9d08e1d21c9d3b135b
二條正則 <jonym@nijoumasanorinoMacBook-Pro-2.local>
2022-09-13 03:13:57 +0900

    コースパターン作成中

--- tool/cource_prm_made.py
+++ tool/cource_prm_made.py
@@ -53,6 +53,65 @@ def madeCourceK(json_data):
 
     f.close
 
+def madeCourceH(json_data):
+    minLengh = json_data["madePrm"]["minLengh"]
+    maxLength = json_data["madePrm"]["maxLength"]
+    minDeg = json_data["madePrm"]["minDeg"]
+    maxDeg = json_data["madePrm"]["maxDeg"]
+    diffPrm = json_data["madePrm"]["diffPrm"]
+    courceR = 0
+    courceLength = 0
+    beforeDeg = 0
+
+    cdir = os.path.dirname(__file__)
+    file_path = cdir+"/courceH_prm.txt"
+    f = open(file_path, 'w')
+    # _c->dC(100,-35 + kaku);
+    for i in range(20):
+      courceLength = randint(minLengh, maxLength)
+      courceLength2 = randint(minLengh, maxLength)
+      courceDeg = randint(min(beforeDeg + diffPrm,maxDeg), maxDeg) if i%2 else randint(minDeg, max(beforeDeg-diffPrm,minDeg))
+      diffDeg = courceDeg - beforeDeg
+      beforeDeg = courceDeg
+      courceR = int(abs(courceLength/(3.14 * (diffDeg/180))))
+
+      f.write("_c->dS("+str(courceLength)+", "+str(courceDeg)+json_data["funcEd"])
+      f.write('\n')
+      f.write("_c->dS("+str(courceLength2)+", "+str(0)+json_data["funcEd"])
+      f.write('\n')
+    f.close
+
+def madeCourceCH(json_data):
+    minLengh = json_data["madePrm"]["minLengh"]
+    maxLength = json_data["madePrm"]["maxLength"]
+    minDeg = json_data["madePrm"]["minDeg"]
+    maxDeg = json_data["madePrm"]["maxDeg"]
+    diffPrm = json_data["madePrm"]["diffPrm"]
+    courceR = 0
+    courceLength = 0
+    beforeDeg = 0
+
+    cdir = os.path.dirname(__file__)
+    file_path = cdir+"/courceCH_prm.txt"
+    f = open(file_path, 'w')
+    # _c->dC(100,-35 + kaku);
+    for i in range(20):
+      courceLength = randint(minLengh, maxLength)
+      courceLength2 = randint(minLengh, maxLength)
+      courceDeg = randint(min(beforeDeg + diffPrm,maxDeg), maxDeg) if i%2 else randint(minDeg, max(beforeDeg-diffPrm,minDeg))
+      diffDeg = courceDeg - beforeDeg
+      beforeDeg = courceDeg
+      courceR = int(abs(courceLength/(3.14 * (diffDeg/180))))
+      if(randint(0,1)==0):
+        f.write("_c->dS("+str(courceLength)+", "+str(courceDeg)+json_data["funcEd"])
+        f.write('\n')
+        f.write("_c->dS("+str(courceLength2)+", "+str(0)+json_data["funcEd"])
+        f.write('\n')
+      else:
+        f.write(json_data["funcSt"]+str(courceR)+", "+str(courceDeg)+json_data["funcEd"])
+        f.write('\n')
+    f.close
+
 def madeCourceCK(json_data):
     minLengh = json_data["madePrm"]["minLengh"]
     maxLength = json_data["madePrm"]["maxLength"]
@@ -90,3 +149,5 @@ json_data = json.load(json_file)
 madeCourceC(json_data)
 madeCourceK(json_data)
 madeCourceCK(json_data)
+madeCourceH(json_data)
+madeCourceCH(json_data)
